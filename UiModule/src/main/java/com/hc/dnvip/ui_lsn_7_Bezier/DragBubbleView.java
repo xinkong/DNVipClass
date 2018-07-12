c89a57dd0cab08b89012eb3fe613b0e66bb52939
xinkong <2624384463@qq.com>
2018-07-12 10:38:55 +0800

    添加readme

--- UiModule/src/main/java/com/hc/dnvip/ui_lsn_7_Bezier/DragBubbleView.java
+++ UiModule/src/main/java/com/hc/dnvip/ui_lsn_7_Bezier/DragBubbleView.java
@@ -1,18 +1,29 @@
 package com.hc.dnvip.ui_lsn_7_Bezier;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.PointFEvaluator;
+import android.animation.ValueAnimator;
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PointF;
 import android.graphics.Rect;
+import android.media.MediaCasStateException;
+import android.os.Build;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.OvershootInterpolator;
 
 import com.hc.dnvip.R;
 
@@ -102,6 +113,33 @@ public class DragBubbleView extends View {
      */
     private float mCenterDistance;
 
+    /**
+     * 气泡爆炸画笔
+     */
+    private Paint mBurstPaint;
+    /**
+     * 气泡爆炸的图片id数组
+     */
+    private int[] mBurstDrawablesArray = {R.drawable.burst_1, R.drawable.burst_2
+            , R.drawable.burst_3, R.drawable.burst_4, R.drawable.burst_5};
+
+    /**
+     * 气泡爆炸的bitmap
+     */
+    private Bitmap[] mBurstBitmapsArray;
+
+    /**
+     * 爆炸绘制区域
+     */
+    private Rect mBurstRect;
+    /**
+     * 绘制消失的图片序号
+     */
+    private int mCurDrawableIndex;
+    /**
+     * 是否在执行气泡爆炸动画
+     */
+    private boolean mIsBurstAnimStart = false;
 
     public DragBubbleView(Context context) {
         this(context, null);
@@ -133,6 +171,16 @@ public class DragBubbleView extends View {
         mTextPaint.setStyle(Paint.Style.FILL);
         mHintMsgSize = new Rect();
         mTextPaint.getTextBounds(mHintMsg, 0, mHintMsg.length(), mHintMsgSize);
+
+        mBurstPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mBurstPaint.setFilterBitmap(true);
+        mBurstRect = new Rect();
+        mBurstBitmapsArray = new Bitmap[mBurstDrawablesArray.length];
+        for (int i = 0; i < mBurstDrawablesArray.length; i++) {
+            //将气泡爆炸的drawable转为bitmap
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), mBurstDrawablesArray[i]);
+            mBurstBitmapsArray[i] = bitmap;
+        }
     }
 
     @Override
@@ -170,24 +218,24 @@ public class DragBubbleView extends View {
             float smallAx = mSmallCircleCenter.x - mSmallCircleRadius * sinValue;
             float smallAy = mSmallCircleCenter.y + mSmallCircleRadius * cosValue;
 
-            canvas.drawText("a", smallAx, smallAy, mTextPaint);
+//            canvas.drawText("a", smallAx, smallAy, mTextPaint);
 
             float smallBx = mSmallCircleCenter.x + mSmallCircleRadius * sinValue;
             float smallBy = mSmallCircleCenter.y - mSmallCircleRadius * cosValue;
-            canvas.drawText("b", smallBx, smallBy, mTextPaint);
+//            canvas.drawText("b", smallBx, smallBy, mTextPaint);
             //大圆CD点坐标
             float bigCx = mBigCircleCenter.x - mBigCircleRadius * sinValue;
             float bigCy = mBigCircleCenter.y + mBigCircleRadius * cosValue;
-            canvas.drawText("c", bigCx, bigCy, mTextPaint);
+//            canvas.drawText("c", bigCx, bigCy, mTextPaint);
 
             float bigDx = mBigCircleCenter.x + mBigCircleRadius * sinValue;
             float bigDy = mBigCircleCenter.y - mBigCircleRadius * cosValue;
-            canvas.drawText("d", bigDx, bigDy, mTextPaint);
+//            canvas.drawText("d", bigDx, bigDy, mTextPaint);
 
             //两个圆中心点坐标(锚点坐标)
             float disX = (mBigCircleCenter.x + mSmallCircleCenter.x) / 2;
             float disY = (mBigCircleCenter.y + mSmallCircleCenter.y) / 2;
-            canvas.drawText("z", disX, disY, mTextPaint);
+//            canvas.drawText("z", disX, disY, mTextPaint);
 
             //绘制曲线
             Path path = new Path();
@@ -202,6 +250,16 @@ public class DragBubbleView extends View {
             drawBigCircle(canvas);
         }
 
+        if (mStateType == STATE_DISAPPEAR && mIsBurstAnimStart) {//消失
+            mBurstRect.set((int) (mBigCircleCenter.x - mBigCircleRadius),
+                    (int) (mBigCircleCenter.y - mBigCircleRadius),
+                    (int) (mBigCircleCenter.x + mBigCircleRadius),
+                    (int) (mBigCircleCenter.y + mBigCircleRadius));
+
+            canvas.drawBitmap(mBurstBitmapsArray[mCurDrawableIndex], null,
+                    mBurstRect, mBurstPaint);
+        }
+
     }
 
     @Override
@@ -209,14 +267,17 @@ public class DragBubbleView extends View {
 
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                mBigCircleCenter.x = event.getX();
-                mBigCircleCenter.y = event.getY();
-                //计算圆心距:
-                mCenterDistance = (float) Math.hypot(mBigCircleCenter.x - mSmallCircleCenter.x, mBigCircleCenter.y - mSmallCircleCenter.y);
-                if (mCenterDistance <= mSeparateDistance * mBigCircleRadius) {//处于连接状态,为后面移动铺路
-                    mStateType = STATE_CONNECT;
-                } else {//超过最大距离,在按下屏幕时应该是静止状态(认为没有点在目标上)
-                    mStateType = STATE_STATIC;
+                if (mStateType == STATE_STATIC) {
+
+                    mBigCircleCenter.x = event.getX();
+                    mBigCircleCenter.y = event.getY();
+                    //计算圆心距:
+                    mCenterDistance = (float) Math.hypot(mBigCircleCenter.x - mSmallCircleCenter.x, mBigCircleCenter.y - mSmallCircleCenter.y);
+                    if (mCenterDistance <= mSeparateDistance * mBigCircleRadius) {//处于连接状态,为后面移动铺路
+                        mStateType = STATE_CONNECT;
+                    } else {//超过最大距离,在按下屏幕时应该是静止状态(认为没有点在目标上)
+                        mStateType = STATE_STATIC;
+                    }
                 }
 
                 //思路2:判断当前点击的xy是否在大圆范围内
@@ -228,12 +289,12 @@ public class DragBubbleView extends View {
 
             case MotionEvent.ACTION_MOVE:
 
-                if (mStateType != STATE_STATIC) {
+                if (mStateType != STATE_STATIC && mStateType != STATE_DISAPPEAR) {
                     mBigCircleCenter.x = event.getX();
                     mBigCircleCenter.y = event.getY();
                     //计算圆心距:
                     mCenterDistance = (float) Math.hypot(mBigCircleCenter.x - mSmallCircleCenter.x, mBigCircleCenter.y - mSmallCircleCenter.y);
-                    if (mCenterDistance <= mSeparateDistance * mBigCircleRadius) {
+                    if (mCenterDistance <= mSeparateDistance * mBigCircleRadius && mStateType != STATE_SEPARATE) {
                         mStateType = STATE_CONNECT;
                     } else {//分离
                         mStateType = STATE_SEPARATE;
@@ -246,11 +307,78 @@ public class DragBubbleView extends View {
                 break;
 
             case MotionEvent.ACTION_UP:
+                if (mStateType == STATE_CONNECT) {
+                    //回弹效果
+                    startBubbleRestAnim();
+                } else if (mStateType == STATE_SEPARATE) {
+                    float x = event.getX();
+                    float y = event.getY();
+                    if(Math.hypot(x - mSmallCircleCenter.x, y - mSmallCircleCenter.y)<mSeparateDistance * mBigCircleRadius ){
+                        mBigCircleCenter.x = x;
+                        mBigCircleCenter.y = y;
+                        mStateType = STATE_CONNECT;
+                        startBubbleRestAnim();
+                    }else {
+                        mStateType = STATE_DISAPPEAR;
+                        //执行消失动画
+                        startBubbleBurstAnim();
+                    }
+                }
 
                 break;
         }
 
         return true;
+    }
+
+    private void startBubbleBurstAnim() {
+        //气泡改为消失状态
+        mIsBurstAnimStart = true;
+        //做一个int型属性动画，从0~mBurstDrawablesArray.length结束
+        ValueAnimator anim = ValueAnimator.ofInt(0, mBurstDrawablesArray.length);
+        anim.setInterpolator(new LinearInterpolator());
+        anim.setDuration(500);
+        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                //设置当前绘制的爆炸图片index
+                mCurDrawableIndex = (int) animation.getAnimatedValue();
+                invalidate();
+            }
+        });
+        anim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                //修改动画执行标志
+                mIsBurstAnimStart = false;
+            }
+        });
+        anim.start();
+
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void startBubbleRestAnim() {
+        ValueAnimator anim = ValueAnimator.ofObject(new PointFEvaluator(),
+                new PointF(mBigCircleCenter.x, mBigCircleCenter.y),
+                new PointF(mSmallCircleCenter.x, mSmallCircleCenter.y));
+
+        anim.setDuration(200);
+        anim.setInterpolator(new OvershootInterpolator(5f));
+        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                mBigCircleCenter = (PointF) animation.getAnimatedValue();
+                invalidate();
+            }
+        });
+        anim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                mStateType = STATE_STATIC;
+            }
+        });
+        anim.start();
     }
 
     /**
